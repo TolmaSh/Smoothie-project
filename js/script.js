4f334059cc1a76286ccaf2aa6cbb82094212b699
TolmaSh <m0v4anua@gmail.com>
2021-04-18 18:31:26 +0300

    Build: add Js folder and file / Feat: add hover for nav and smooth scroll after click on nav link

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.nav__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+			
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
