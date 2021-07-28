e1ed4e35e80cf9b4eb7954cdac7686f774123233
TolmaSh <m0v4anua@gmail.com>
2021-07-28 19:58:42 +0300

    Refactoring: clear code

--- js/script.js
+++ js/script.js
@@ -63,6 +63,7 @@ if (animItems.length > 0) {
 
 			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
 				animItem.classList.add('_active');
+			
 			} else {
 				if (!animItem.classList.contains('_anim-no-hide')) {
 					animItem.classList.remove('_active');
